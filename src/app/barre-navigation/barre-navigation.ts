cc711dcc5cd2d05af3717b2beb312a6e86b639ac
Soumeyatou10 <Soumeyatouy@gmail.com>
2026-05-23 12:24:03 +0100

    mise a jour

--- src/app/barre-navigation/barre-navigation.ts
+++ src/app/barre-navigation/barre-navigation.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-barre-navigation',
-  standalone: true, 
+  standalone: true,
   imports: [],
   templateUrl: './barre-navigation.html',
   styleUrl: './barre-navigation.css',
 })
 export class BarreNavigation {
+ menuOuvert = false;
 
+  toggleMenu() {
+    this.menuOuvert = !this.menuOuvert;
+  }
+
+  fermerMenu() {
+    this.menuOuvert = false;
+  }
 }
